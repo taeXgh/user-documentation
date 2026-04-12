2396687e8e26da832991d18556d4ff7b35bed586
taeXgh <talopalo10@gmail.com>
2026-04-11 20:01:15 -0400

    Update theme configuration with new project links and remove chat section

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,12 +4,9 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>My Project</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/taeXgh/user-documentation',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/taeXgh/user-documentation',
   footer: {
     text: 'Nextra Docs Template',
   },
