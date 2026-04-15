5e1ba8f262ed9ea566ce8e7b8388ed6556c5171f
taeXgh <talopalo10@gmail.com>
2026-04-15 00:33:53 -0400

    Update logo in theme configuration to reflect project name

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>ATCAP Documentation</span>,
   project: {
     link: 'https://github.com/taeXgh/user-documentation',
   },
